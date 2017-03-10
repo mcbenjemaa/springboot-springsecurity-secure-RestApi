be43344f49461be735732c055efa08404e255ac2
Mohamed chiheb ben jemaa <med.chiheb@live.com>
2017-03-10 10:49:33 +0100

    springboot-springsecurity

---
+++ springbootfirstapp/src/main/java/springbootfirstapp/SecuirtyConfig.java
@@ -0,0 +1,28 @@
+package springbootfirstapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecuirtyConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .csrf().disable()
+        .authorizeRequests()
+        
+          .antMatchers(HttpMethod.POST, "/api/**").authenticated()
+          .antMatchers(HttpMethod.PUT, "/api/**").authenticated()
+          .antMatchers(HttpMethod.DELETE, "/api/**").authenticated()
+          .antMatchers(HttpMethod.GET, "/api/**").authenticated()
+          .anyRequest().permitAll()
+          .and()
+        .httpBasic().and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+  }
